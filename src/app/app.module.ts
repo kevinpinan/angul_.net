69870d081e103dc77a71f41af8b870e557175eec
kevinpinan <kevin_pealth@hotmail.com>
2023-05-03 14:35:33 -0500

    iniciando

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AgregarEditarMascotaComponent,
+    ListadoMascotaComponent,
+    VerMascotaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-mascota',
+  templateUrl: './listado-mascota.component.html',
+  styleUrls: ['./listado-mascota.component.css']
+})
+export class ListadoMascotaComponent {
+
+}
